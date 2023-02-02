3c1d178a185056a657a1402048e912716d7498f3
quanguway <quangnguyen.uway@gmail.com>
2023-02-02 17:38:02 +0700

    fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const configAPI = {
   headers: {
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? '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'}`,
   }
 }
 
