326468ff413d4332447616afde8f4a9fb167822f
quanguway <quangnguyen.uway@gmail.com>
2023-02-03 09:23:55 +0700

    fix hi to hello

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -78,9 +78,10 @@ export function Chat() {
     setMessages(newMessages)
     const last10messages = newMessages.slice(-10)
 
+    message.replace('hi', 'hello')
+
     const {apiKey} = router.query;
     console.log(process.env.NEXT_OPENAI_API_KEY)
-
     const response = await axios.post('https://api.openai.com/v1/completions', {
     	"model": "text-davinci-003",
 		  "prompt": `${message}`,
