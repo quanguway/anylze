685aab8cf8f8f072defeb9b5347547c42a98a187
quanguway <quangnguyen.uway@gmail.com>
2023-02-04 23:54:55 +0700

    fix api write-file

--- pages/api/write-file.js
+++ pages/api/write-file.js
@@ -1,25 +1,25 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
-export default async function (req, res) {
-  try{
-    // req.setHeader("Content-Type", "application/json");
-    const pathName = path.join(__dirname, "../../../../assets/log/chatLog.json");
-  //   fs.readFile(pathName, function (err, data) {
-  //     var json = JSON.parse(data.toString())
-  //     json.push(req.body)
-  //     fs.writeFile(pathName, JSON.stringify(json),(data)=>{})
-  //     return { message: 'log file complie' };
-  //   })
-  // } catch (err) {
-  //   console.log(err)
-  // }
-  const http = require("http");
-  const file = fs.createWriteStream("file.docx");
+// export default async function (req, res) {
+//   try{
+//     // req.setHeader("Content-Type", "application/json");
+//     const pathName = path.join(__dirname, "../../../../assets/log/chatLog.json");
+//   //   fs.readFile(pathName, function (err, data) {
+//   //     var json = JSON.parse(data.toString())
+//   //     json.push(req.body)
+//   //     fs.writeFile(pathName, JSON.stringify(json),(data)=>{})
+//   //     return { message: 'log file complie' };
+//   //   })
+//   // } catch (err) {
+//   //   console.log(err)
+//   // }
+//   const http = require("http");
+//   const file = fs.createWriteStream("file.docx");
 
-  http.get("http://www.example.com/test.docx", response => {
-    response.pipe(file);
-  });
+//   http.get("http://www.example.com/test.docx", response => {
+//     response.pipe(file);
+//   });
   
-  return res.status(200).json({ message: 'log file complie' });
-}
+//   return res.status(200).json({ message: 'log file complie' });
+// }
